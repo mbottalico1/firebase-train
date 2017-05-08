27d8800af7c02cd4b35e16c6146a9c45b2eec5d9
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-05-08 13:30:22 -0400

    added firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,24 @@
+var config = {
+    apiKey: "AIzaSyB8MDc3DP4QR5tkZ5qWZlNffrrYIJW1XvU",
+    authDomain: "train-schedule-5ad4b.firebaseapp.com",
+    databaseURL: "https://train-schedule-5ad4b.firebaseio.com",
+    projectId: "train-schedule-5ad4b",
+    storageBucket: "train-schedule-5ad4b.appspot.com",
+    messagingSenderId: "1033893274538"
+  };
+  
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
