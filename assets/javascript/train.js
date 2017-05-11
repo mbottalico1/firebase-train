c733455d918f3cc3626e08c211ce5ee15be0f79a
Michael Bottalico <michaelbottalico@Michaels-MBP.home>
2017-05-11 12:24:47 -0400

    update javascript/fixed

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -5,45 +5,45 @@ var config = {
     projectId: "train-schedule-5ad4b",
     storageBucket: "train-schedule-5ad4b.appspot.com",
     messagingSenderId: "1033893274538"
-  };
-  
-  firebase.initializeApp(config);
+};
 
-  var database = firebase.database();
+firebase.initializeApp(config);
 
-  $('.btn-default').on('click', function(event) {
-  		event.preventDefault();
+var database = firebase.database();
 
-  	var trainName = $('#InputTrainName').val().trim();
-  	var trainDestination = $('#InputDestination').val().trim();
-  	var trainTime = moment($("#InputTrainTime").val().trim(), "DD/MM/YY").format("X");
-  	var trainFrequency = $('#InputFrequency').val().trim();
+$('.btn-default').on('click', function(event) {
+    event.preventDefault();
 
-  	var newTrain = {
-  		name: trainName,
-  		destination: trainDestination,
-  		time: trainTime,
-  		frequency: trainFrequency
-  	};
+    var trainName = $('#InputTrainName').val().trim();
+    var trainDestination = $('#InputDestination').val().trim();
+    var trainTime = $("#InputTrainTime").val().trim();
+    var trainFrequency = $('#InputFrequency').val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    };
 
 
-  	database.ref().push(newTrain);
+    database.ref().push(newTrain);
 
-  	console.log(newTrain.name);
-  	console.log(newTrain.destination);
-  	console.log(newTrain.time);
-  	console.log(newTrain.frequency);
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
 
-  	alert('Train Station Added!');
+    alert('Train Station Added!');
 
     $('#InputTrainName').val("");
     $('#InputDestination').val("");
     $('#InputTrainTime').val("");
     $('#InputFrequency').val("");
 
-  })
+})
 
-  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
     console.log(childSnapshot.val());
 
@@ -59,23 +59,30 @@ var config = {
     console.log(trainFrequency);
 
 
-    var timeTrain = moment().add(1, 'days').calendar();
+    var hour = moment().format('H');
+    var min = moment().format('m');
+    var trainHour = moment(trainTime, "HH:mm").format('H');
+    var trainMin = moment(trainTime, "HH:mm").format('m');
 
-    $('.table > tbody').append("<tr><td>" + trainName + "<tr2><td>" + trainDestination + "<tr3><td>" + trainFrequency + "<tr4><td>" + timeTrain);
+    var momentTrain = (trainMin * 1) + (trainHour * 60);
+    var momentTime = (hour * 60) + (min * 1);
 
-  })
+    var difference = momentTime - momentTrain;
+
+    var frequencySince = Math.floor(difference / trainFrequency);
+
+    var nextArrival = ((frequencySince + 1) * trainFrequency) + momentTrain;
+
+    if (momentTrain < momentTime) {
+        var minutesAway = nextArrival - momentTime;
+        var nextArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
+    } else {
+        nextArrival = trainTime;
+        minutesAway = momentTrain - momentTime;
+    };
 
 
 
+    $('.table > tbody').append("<tr><td>" + trainName + "</th><th>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + trainTime + "</td><td>" + minutesAway + "<td><tr>");
 
-
-
-
-
-
-
-
-
-
-
-  
+})
